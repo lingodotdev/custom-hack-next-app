388dbedd1cb62c890872ae35aad4f85646892470
michael <mike.michaelmilstead@gmail.com>
2025-07-02 16:35:11 -0700

    update registered components

--- src/app/tambo/page.tsx
+++ src/app/tambo/page.tsx
@@ -1,9 +1,20 @@
-import Container from "@/components/tambo/container";
+"use client";
+import { MessageThreadFull } from "@/components/tambo/message-thread-full";
+import { components, tools } from "@/lib/tambo/tambo";
+import { TamboProvider } from "@tambo-ai/react";
 
 export default function TamboPage() {
   return (
     <div className="flex flex-col gap-8 items-center min-h-screen py-8 px-4 max-w-3xl mx-auto">
-      <Container />
+      <TamboProvider
+        apiKey={process.env.NEXT_PUBLIC_TAMBO_API_KEY!}
+        components={components}
+        tools={tools}
+      >
+        <div className="p-4 tambo-theme">
+          <MessageThreadFull />
+        </div>
+      </TamboProvider>
     </div>
   );
 }
--- src/lib/tambo/tambo.ts
+++ src/lib/tambo/tambo.ts
@@ -9,6 +9,7 @@
  */
 
 import Firecrawl from "@/components/firecrawl";
+import ResendEmailForm from "@/components/resend/form";
 import type { TamboComponent } from "@tambo-ai/react";
 import { TamboTool } from "@tambo-ai/react";
 import { z } from "zod";
@@ -21,20 +22,38 @@ import { z } from "zod";
  * can be controlled by AI to dynamically fetch data based on user interactions.
  */
 
-export const tools: TamboTool[] = [];
+export const tools: TamboTool[] = [
+  {
+    name: "test-tool",
+    description: "A tool to test the tooling",
+    tool: async (name: string) => {
+      //artificial delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return `Hello ${name}`;
+    },
+    toolSchema: z.function().args(z.string()).returns(z.string()),
+  },
+];
 
 /**
  * components
  *
- * This array contains all the Tambo components that are registered for use within the application.
+ * This array contains all the components that are registered for use by Tambo within the application.
  * Each component is defined with its name, description, and expected props. The components
  * can be controlled by AI to dynamically render UI elements based on user interactions.
  */
 export const components: TamboComponent[] = [
   {
     name: "firecrawl",
-    description: "A tool to scrape a website and return the results",
+    description:
+      "A form to enter a url and scrape a website using firecrawl and return the results",
     component: Firecrawl,
+    propsSchema: z.object({}),
+  },
+  {
+    name: "resend",
+    description: "A form to send an email using Resend",
+    component: ResendEmailForm,
     propsSchema: z.object({}),
   },
   // Add more components here
